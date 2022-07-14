c4eb865c5506eb070ec0f32efe98dfe87da8df28
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-14 21:21:35 +0530

    Added : Find Maximum from three Integers.

--- com/bridgelabz/Generics/PrintArray.java
+++ com/bridgelabz/Generics/PrintArray.java
@@ -2,10 +2,22 @@ package com.bridgelabz.Generics;
 
 public class PrintArray<T> {
 
+	public void toPrint(T[] arr) {
+		System.out.println("Integer Array:");
+		for (T t : arr) {
+			System.out.print(t + "  ");
+		}
+		System.out.print("\n");
+	}
 	public static void main(String[] args) {
 		//printing welcome message
+		MaximumTest max = new MaximumTest();
 		System.out.println("Welcome to Java Generics Program.");
-
+		Integer[] integerArr = new Integer[] { 1,2,3,4,6,7,8,9};
+		PrintArray<Integer> printArr = new PrintArray<Integer>();
+		printArr.toPrint(integerArr);
+		max.findIntMax();
+		
 	}
 
 }
