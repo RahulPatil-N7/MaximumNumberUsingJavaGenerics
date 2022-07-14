a36edff23c6656b5a34a64885be63f76c41efee4
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-14 20:26:09 +0530

    Added : Welcome message.

---
+++ com/bridgelabz/Generics/PrintArray.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Generics;
+
+public class PrintArray<T> {
+
+	public static void main(String[] args) {
+		//printing welcome message
+		System.out.println("Welcome to Java Generics Program.");
+
+	}
+
+}
