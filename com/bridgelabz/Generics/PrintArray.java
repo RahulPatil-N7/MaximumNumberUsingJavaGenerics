7873e29a387fb1c0db0051e00862ab38a8c040e8
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-31 16:31:51 +0530

    Refactored : generic method to print max.

--- com/bridgelabz/Generics/PrintArray.java
+++ com/bridgelabz/Generics/PrintArray.java
@@ -10,9 +10,7 @@ public class PrintArray<T> {
 		Maximum max = new Maximum();
 		
 		//using Maximum object max to call functions
-		System.out.println("The Maximum integer num is:" + max.findMaxList(Arrays.asList(39, 57, 88, 65, 12, 98)));
-		System.out.println("The Maximum float num is:" + max.findMaxList(Arrays.asList(5.4, 3.7, 8.9, 1.9, 9.9, 4.2)));
-		System.out.println("The Maximum String  is:" + max.findMaxList(Arrays.asList("Banana", "Apple", "Peach", "Mango", "Orange")));
+		max.printMax();
 	}
 
 }
