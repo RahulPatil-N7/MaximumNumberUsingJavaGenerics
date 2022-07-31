872cfa06c6ebf68fdaad9d3e60cd5277d44b1ef2
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-31 16:20:31 +0530

    Refactored : Ability find max for more than 3 elements.

--- com/bridgelabz/Generics/PrintArray.java
+++ com/bridgelabz/Generics/PrintArray.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.Generics;
 
+import java.util.Arrays;
+
 public class PrintArray<T> {
 
 	public static void main(String[] args) {
@@ -8,9 +10,9 @@ public class PrintArray<T> {
 		Maximum max = new Maximum();
 		
 		//using Maximum object max to call functions
-		max.findIntMax();
-		max.findFloatMaximum();
-		max.findStringMaximum();
+		System.out.println("The Maximum integer num is:" + max.findMaxList(Arrays.asList(39, 57, 88, 65, 12, 98)));
+		System.out.println("The Maximum float num is:" + max.findMaxList(Arrays.asList(5.4, 3.7, 8.9, 1.9, 9.9, 4.2)));
+		System.out.println("The Maximum String  is:" + max.findMaxList(Arrays.asList("Banana", "Apple", "Peach", "Mango", "Orange")));
 	}
 
 }
